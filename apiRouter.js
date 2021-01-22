1cc0ecf168a8a92e1b2414f5fe1e4dd7d43afc8e
Dexter Leng <dexterlengcs@gmail.com>
2021-01-22 16:23:01 +0800

    set up multer and talent upload api

--- apiRouter.js
+++ apiRouter.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: './uploads/',
+});
+
 /**
  * @swagger
  * /healthcheck:
@@ -21,5 +26,33 @@ const router = express.Router()
 router.use('/healthcheck', function (req, res, next) {
     res.status(200).send('API Healthy');
 })
+
+// taken from https://stackoverflow.com/a/60408823/10390454
+const talentMulterMiddleware = multer({
+  storage: storage,
+  limits: {
+    fieldSize: 1048576
+  },
+  fileFilter: function(req, file, cb){
+    const filetypes = /jpeg|jpg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('Error: Images Only!');
+    }
+  }
+}).single('talent');
+
+router.post('/talents/', talentMulterMiddleware, async (req, res, next) => {
+  try {
+    const profilePicture = req.file;
+    const { name, description } = req.body;
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
