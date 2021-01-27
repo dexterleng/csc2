423a0c83c3e4f598a166d29276cecba251bcfb6c
Lolepopie <8401103+lolepop@users.noreply.github.com>
2021-01-27 14:10:20 +0800

    base documentation setup

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
 const viewRouter = require('./viewRouter');
 const apiRouter = require('./apiRouter');
+
+const PORT = 3001;
+
+const options = {
+    swaggerDefinition: require("./swaggerDefinition"),
+    apis: ["./*.js"]
+}
+
+const swaggerSpec = swaggerJSDoc(options);
+
 const app = express();
 
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api', apiRouter);
 app.set('view engine', 'ejs');
 app.use(viewRouter);
-app.use('/api', apiRouter);
 
-const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+    console.log(`Listening on port ${PORT}`)
 })
