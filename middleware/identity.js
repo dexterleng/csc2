a516710225c1918aaea9a47a2e8c888654e79d20
Lolepopie <8401103+lolepop@users.noreply.github.com>
2021-02-06 18:58:09 +0800

    test flag for fake identity

--- middleware/identity.js
+++ middleware/identity.js
@@ -1,14 +1,28 @@
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../env_constants");
+const dynamodb = require("../lib/dynamodb");
+const { getSubscription } = require("../lib/stripe");
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 
-    // res.locals.user = {
-    //     username: "testuser1",
-    //     stripeCustomerId: "cus_Is5uFEpKQjzGZ0",
-    //     type: "premium"
-    // };
-    // return next();
+    // override users for testing
+    if (parseInt(process.env.TESTING))
+    {
+        const username = "testuser1";
+
+        const user = (await dynamodb.getTableData({
+            TableName: "users",
+            Key: { username }
+        })).Item;
+    
+        const { currentPlan } = await getSubscription(user.stripeCustomerId);
+        res.locals.user = {
+            username,
+            stripeCustomerId: user.stripeCustomerId,
+            type: currentPlan
+        };
+        return next();
+    }
 
     try {
         const authToken = req.cookies.token;
